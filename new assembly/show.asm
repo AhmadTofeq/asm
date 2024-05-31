609f37d94fe874189f538d4571f3fe61a2e7b5f9
Ahmad Tofeq <120684065+AhmadTofeq@users.noreply.github.com>
2024-05-31 21:20:34 +0300

    Update show.asm

--- new assembly/show.asm
+++ new assembly/show.asm
@@ -1,12 +1,12 @@
 .model small
 .data
-wsha db 'kakm','$'
+string db 'input is: ','$'
 .stack
 .code
 main:
     mov ax, @data
     mov ds,ax
-    lea dx,[wsha]
+    lea dx,[string]
     mov ah,09h
     int 21h
     mov ah,4ch
